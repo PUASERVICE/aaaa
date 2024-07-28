0920214f66f09dd91ea4b680a4e0fc66bfcded36
PUASERVICE <xh509509@outlook.com>
2024-07-29 00:28:42 +0800

    123

--- static/custom.js
+++ static/custom.js
@@ -1,1 +1,1 @@
-console.log('window.$crisp=[];window.CRISP_WEBSITE_ID="39d1c685-4d4d-4d9b-9b6f-669a1a965982";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();')
+console.log('这里面随便写自定义js脚本')
